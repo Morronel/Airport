3410af0b872ba627cce9f2a40ea44411346e3ea1
Morronel <zahar.kogut@gmail.com>
2018-10-30 22:26:15 +0200

    Created main body for Flight class

--- src/com/morronel/Flight.java
+++ src/com/morronel/Flight.java
@@ -1,4 +1,35 @@
 package com.morronel;
 
 public class Flight {
+    private String planeId;
+    private String departureCode;
+    private String destinationCode;
+
+    public Flight(){
+
+    }
+
+    public String getPlaneId() {
+        return planeId;
+    }
+
+    public void setPlaneId(String planeId) {
+        this.planeId = planeId;
+    }
+
+    public String getDepartureCode() {
+        return departureCode;
+    }
+
+    public void setDepartureCode(String departureCode) {
+        this.departureCode = departureCode;
+    }
+
+    public String getDestinationCode() {
+        return destinationCode;
+    }
+
+    public void setDestinationCode(String destinationCode) {
+        this.destinationCode = destinationCode;
+    }
 }
